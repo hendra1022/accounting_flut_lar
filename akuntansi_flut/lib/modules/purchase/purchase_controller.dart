c2fc9c72c1f630d31538248fa441926f249a8961
Kevin Angka Wijaya <kevinaw46@gmail.com>
2023-04-03 14:20:33 +0700

    v1

--- akuntansi_flut/lib/modules/purchase/purchase_controller.dart
+++ akuntansi_flut/lib/modules/purchase/purchase_controller.dart
@@ -1,3 +1,52 @@
-import '../../commons/base_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class PurchaseController extends BaseController {}
+import '../../commons/base_controller.dart';
+import '../../services/model/item_model.dart';
+import '../customer/component/table_data_source.dart';
+
+class PurchaseController extends BaseController {
+  final CustomerDataTableSource dataSource = CustomerDataTableSource();
+
+  // filter by name
+  String selectedFilterBy = 'Document No';
+  final TextEditingController itemSearchController = TextEditingController();
+  final List<String> filterByItems = [
+    'Document No',
+    'Customer Name',
+  ];
+
+  // filter by date
+  DateTime startDate = DateTime.now();
+  String startDateView = "";
+  DateTime endDate = DateTime.now();
+  String endDateView = "";
+
+  @override
+  Future<void> onInit() async {
+    isLoading = true;
+    await Future.delayed(const Duration(seconds: 1));
+    startDateView = DateFormat("dd-MM-yyyy").format(startDate);
+    endDateView = DateFormat("dd-MM-yyyy").format(endDate);
+    isLoading = false;
+    update();
+
+    super.onInit();
+  }
+
+  void sortData<T>(Comparable<T> Function(ItemModel user) getField, int colIndex, bool ascending) {
+    dataSource.sort<T>(getField, ascending);
+  }
+
+  void updateStartDate(DateTime temp) {
+    startDate = temp;
+    startDateView = DateFormat("dd-MM-yyyy").format(startDate);
+    update();
+  }
+
+  void updateEndDate(DateTime temp) {
+    endDate = temp;
+    endDateView = DateFormat("dd-MM-yyyy").format(endDate);
+    update();
+  }
+}
--- akuntansi_flut/lib/modules/purchase/purchase_page.dart
+++ akuntansi_flut/lib/modules/purchase/purchase_page.dart
@@ -1,3 +1,5 @@
+import 'package:akuntansi_flut/modules/purchase/purchase.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,16 +23,18 @@ class PurchasePage extends StatelessWidget {
   Widget _body() {
     return Container(
       width: double.infinity,
-      padding: EdgeInsets.all(paddingSmall),
+      padding: const EdgeInsets.all(paddingSmall),
       child: Column(
         children: [
-          _header(),
-          SizedBox(
+          const Header(),
+          const SizedBox(
             height: marginSmall,
           ),
           Expanded(
             child: ListView(
-              children: const [],
+              children: const [
+                Filter(),
+              ],
             ),
           ),
         ],
@@ -41,8 +45,8 @@ class PurchasePage extends StatelessWidget {
   Widget _header() {
     return Container(
       width: double.infinity,
-      decoration: BoxDecoration(color: VColor.primary, borderRadius: BorderRadius.all(Radius.circular(radiusMedium))),
-      padding: EdgeInsets.all(paddingMedium),
+      decoration: const BoxDecoration(color: VColor.primary, borderRadius: BorderRadius.all(Radius.circular(radiusMedium))),
+      padding: const EdgeInsets.all(paddingMedium),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -63,17 +67,17 @@ class PurchasePage extends StatelessWidget {
                     Icons.arrow_right,
                     color: VColor.white,
                   ),
-                  VText(
+                  const VText(
                     "Purchase",
                     fontSize: textSizeMedium,
                     color: VColor.white,
                   ),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: marginSuperSmall,
               ),
-              VText(
+              const VText(
                 "Purchase",
                 fontSize: textSizeLarge,
                 color: VColor.white,
@@ -96,5 +100,218 @@ class PurchasePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(color: VColor.primary, borderRadius: BorderRadius.all(Radius.circular(radiusMedium))),
+      padding: const EdgeInsets.all(paddingMedium),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  VText(
+                    "Dashboard",
+                    fontSize: textSizeMedium,
+                    color: VColor.white,
+                    onPressed: () {
+                      Get.back();
+                    },
+                  ),
+                  const Icon(
+                    Icons.arrow_right,
+                    color: VColor.white,
+                  ),
+                  const VText(
+                    "Purchase",
+                    fontSize: textSizeMedium,
+                    color: VColor.black,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: marginSuperSmall,
+              ),
+              const VText(
+                "Purchase",
+                fontSize: textSizeLarge,
+                color: VColor.white,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              VButton(
+                "CREATE",
+                buttonColor: VColor.secondary,
+                leftIcon: const Icon(
+                  Icons.add,
+                  color: VColor.white,
+                ),
+                onPressed: () {
+                  // VNavigation().toCustomerCreatePage();
+                },
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Filter extends StatelessWidget {
+  const Filter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: VColor.white),
+      padding: const EdgeInsets.all(marginMedium),
+      child: Wrap(
+        children: [
+          dateFilter(),
+          const SizedBox(
+            width: 30,
+          ),
+          filterByDropdown(),
+        ],
+      ),
+    );
+  }
+
+  Widget dateFilter() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: marginMedium),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const VText("Date"),
+          const SizedBox(
+            width: marginSmall,
+          ),
+          GetBuilder<PurchaseController>(
+            builder: (controller) => GestureDetector(
+              onTap: () => showDatePickerStartDate(controller),
+              child: VText(
+                controller.startDateView,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: marginSmall,
+          ),
+          const VText("-"),
+          const SizedBox(
+            width: marginSmall,
+          ),
+          GetBuilder<PurchaseController>(
+            builder: (controller) => VText(
+              controller.endDateView,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget filterByDropdown() {
+    return Container(
+      width: 700,
+      decoration: const BoxDecoration(
+        color: VColor.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(radiusMedium),
+        ),
+      ),
+      child: GetBuilder<PurchaseController>(
+        builder: (controller) => Row(
+          children: [
+            DropdownButton2(
+              hint: const Padding(
+                padding: EdgeInsets.only(left: paddingSuperSmall),
+                child: VText(
+                  'Select Item',
+                  fontSize: textSizeMedium,
+                  color: VColor.grey1,
+                ),
+              ),
+              items: controller.filterByItems
+                  .map(
+                    (item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: paddingSuperSmall),
+                        child: VText(
+                          item,
+                          fontSize: textSizeMedium,
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+              value: controller.selectedFilterBy,
+              onChanged: (value) {
+                controller.selectedFilterBy = value as String;
+                controller.update();
+              },
+              buttonStyleData: const ButtonStyleData(
+                height: 40,
+                width: 150,
+              ),
+              menuItemStyleData: const MenuItemStyleData(
+                height: 40,
+              ),
+            ),
+            const SizedBox(
+              width: marginSmall,
+            ),
+            SizedBox(
+              width: 250,
+              child: VInputText(
+                hint: "Search item by ${controller.selectedFilterBy}",
+                textEditingController: controller.itemSearchController,
+                autoFocus: false,
+              ),
+            ),
+            const SizedBox(
+              width: marginSmall,
+            ),
+            VButton(
+              "Filter",
+              leftIcon: const Icon(
+                Icons.search,
+                color: VColor.white,
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> showDatePickerStartDate(PurchaseController controller) async {
+    final DateTime? picked = await showDatePicker(
+      context: Get.context!,
+      initialDate: controller.startDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2030),
+    );
+    if (picked != null && picked != controller.startDate) {
+      controller.updateStartDate(picked);
+    }
   }
 }
